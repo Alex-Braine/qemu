ee87550de1d239d4d7d9eba9b265a8750587511c
Alex-Braine <alex.braine.lu@gmail.com>
2021-12-18 22:35:17 +0100

    upd

--- scripts/build_dependencies.sh
+++ scripts/build_dependencies.sh
@@ -525,9 +525,9 @@ build_spice_client () {
 #    build_mozjs78 $FF78
     build $EXPAT
     meson_build $LIBPSL
-#    meson_build $POLKIT
-    meson_build $SYSPROF
-    meson_build $SOUP_SRC -Dtests=false
+#-    meson_build $POLKIT
+#-    meson_build $SYSPROF
+    meson_build $SOUP_SRC -Dtests=false -Dsysprof=disabled
     meson_build $PHODAV_SRC
     meson_build $SPICE_CLIENT_SRC
 }
