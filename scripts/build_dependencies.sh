46a3d7ec09d9f414c16b29353e5bed8b00d9fc91
Alex-Braine <alex.braine.lu@gmail.com>
2021-12-19 10:53:20 +0100

    upd

--- scripts/build_dependencies.sh
+++ scripts/build_dependencies.sh
@@ -434,28 +434,28 @@ build_angle () {
 }
 
 build_qemu_dependencies () {
-#    build $FFI_SRC
+    build $FFI_SRC
     build $ICONV_SRC
     build $GETTEXT_SRC --disable-java
-#    build $PNG_SRC
-#    build $JPEG_TURBO_SRC
+    build $PNG_SRC
+    build $JPEG_TURBO_SRC
     meson_build $GLIB_SRC -Dtests=false
-#    build $GPG_ERROR_SRC
-#    build $GCRYPT_SRC
+    build $GPG_ERROR_SRC
+    build $GCRYPT_SRC
     build $PIXMAN_SRC
-#    build_openssl $OPENSSL_SRC
-#    build $OPUS_SRC
+    build_openssl $OPENSSL_SRC
+    build $OPUS_SRC
     meson_build $SPICE_PROTOCOL_SRC
     build $SPICE_SERVER_SRC
     # USB support
-#    if [ -z "$SKIP_USB_BUILD" ]; then
-#        build $USB_SRC
-#        meson_build $USBREDIR_SRC
-#    fi
+    if [ -z "$SKIP_USB_BUILD" ]; then
+        build $USB_SRC
+        meson_build $USBREDIR_SRC
+    fi
     # GPU support
-#    build_angle
-#    meson_build $EPOXY_REPO -Dtests=false -Dglx=no -Degl=yes
-#    meson_build $VIRGLRENDERER_REPO -Dtests=false
+    build_angle
+    meson_build $EPOXY_REPO -Dtests=false -Dglx=no -Degl=yes
+    meson_build $VIRGLRENDERER_REPO -Dtests=false
 }
 
 build_qemu () {
