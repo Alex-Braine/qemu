52d0fc6c5b9307d574fbabfbcf42b7f96dc10576
Alex-Braine <alex.braine.lu@gmail.com>
2021-12-21 08:48:12 +0100

    upd

--- scripts/build_dependencies.sh
+++ scripts/build_dependencies.sh
@@ -153,6 +153,7 @@ download_all () {
     download $SYSPROF
     download $SOUP_SRC
     download $PHODAV_SRC
+    download $GRAPHENE
     download $GTK
     download $SPICE_CLIENT_SRC
     download $QEMU_SRC
@@ -535,7 +536,8 @@ build_spice_client () {
     build $SOUP_SRC --without-gnome --without-krb5-config --enable-shared=no --disable-tls-check
     build $PHODAV_SRC
     #meson_build $SPICE_CLIENT_SRC -Dpolkit=disabled
-    #meson_build $GTK -Dx11-backend=false -Dwayland-backend=false -Dwin32-backend=false -Dmedia-gstreamer=disabled -Dprint-cups=disabled -Df16c=disabled -Dintrospection=disabled -Ddemos=false -Dbuild-examples=false -Dbuild-tests=false 
+    meson_build $GRAPHENE -Dinstalled_tests=false -Dtests=false -Dintrospection=false
+    meson_build $GTK -Dx11-backend=false -Dwayland-backend=false -Dwin32-backend=false -Dmedia-gstreamer=disabled -Dprint-cups=disabled -Df16c=disabled -Dintrospection=disabled -Ddemos=false -Dbuild-examples=false -Dbuild-tests=false 
     build $SPICE_CLIENT_SRC
 }
 
